5364fe654bebef4e941ad177842d1e0125925670
alexiscalvi <alex-goal73@hotmail.fr>
2016-01-07 09:50:42 +0100

    ajout de combat et tour

--- RPGMaladie/src/rpgmaladie/Tour.java
+++ RPGMaladie/src/rpgmaladie/Tour.java
@@ -1,17 +1,18 @@
 package rpgmaladie;
 
+import java.util.Random;
+
 public class Tour {
     public int numeroTour=1;
+    private int proba;
+    Random commence=new Random();//proba pour savoir qui commence
     
     
     
-    public Tour(){
+    public Tour(int tour){
+        this.numeroTour=tour;
     }
     
-    public void implementeTour(){
-        numeroTour=numeroTour+1;
-        System.out.println("Tour suivant");
-    }
     
     public int getTour(){
         return numeroTour;
@@ -20,4 +21,19 @@ public class Tour {
     public void raz(){
         numeroTour=0;
     }
+    
+            //On part du principe que la note de dexterité va determiner la proba
+        //de commencer.
+    public boolean JoueurEstPremier(){//(note de dexte limitée a 30, donc proba va de 30à90)
+        //sauf si on a recu des malus.
+        proba=perso.getDexterite()*3;//perso a recuperer apres avoir été construit dans Combat( composition)
+        int varAlea=(int)(Math.random()*100);
+        if (varAlea<proba){
+            return false;
+        }
+        else{
+            return true;
+        }
+         
+        }
 }
